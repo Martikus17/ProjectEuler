277021eb81487f55600e3c19c3ac59a2aa71eac3
Martikus17 <martikaschwan@yahoo.de>
2015-08-06 09:48:09 +0200

    Problem 6 solved
    Change-Id: I0f60a75e97508711ae37250ed2b4f93c277ceede
    Signed-off-by: Martikus17 <martikaschwan@yahoo.de>

---
+++ ProjectEuler/src/problem_6/Problem_6.java
@@ -0,0 +1,26 @@
+package problem_6;
+
+public class Problem_6 {
+	
+	public static void main(String[] args){
+	
+		int sumSquare = 0;
+		int squareSum = 0;
+	
+	
+		for (int i=1; i<=100; i++){
+			sumSquare = sumSquare + i;
+			squareSum = squareSum + getSquare(i);
+		}
+		sumSquare = getSquare(sumSquare);
+		int dif = sumSquare - squareSum;
+		
+		System.out.print("Differenz = " + dif);
+	}
+	
+		
+	public static int getSquare(int x){
+		return x*x;
+	}
+
+}
