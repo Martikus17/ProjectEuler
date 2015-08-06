7369e2ace89f56c862f17c5e8f1ac908ca91d840
Martikus17 <martikaschwan@yahoo.de>
2015-08-06 09:34:09 +0200

    Project 5 solved
    Change-Id: I66f3f1ad91e423735750a15e82dd0400799511a8
    Signed-off-by: Martikus17 <martikaschwan@yahoo.de>

---
+++ ProjectEuler/src/problem_5/Problem_5.java
@@ -0,0 +1,18 @@
+package problem_5;
+
+public class Problem_5 {
+
+	public static void main(String[] args) {
+		
+		int i = 20;
+		
+		while (true){
+			//simplest way
+			if (i%20==0 && i%19==0 && i%18==0 && i%17==0 && i%16==0 && i%15==0 && i%14==0 && i%13==0 && i%12==0 && i%11==0){
+				System.out.print(i);
+				break;
+			}
+			i++;
+		}
+	}
+}
